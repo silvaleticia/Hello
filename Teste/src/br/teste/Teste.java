943bd9531a6027dfdb0fd5ce5778b6d5c7934cd8
leticia.silva <leticia.silva@semad00771928.semarh.goias.intra>
2019-04-25 10:40:28 -0300

    kk

--- Teste/src/br/teste/Teste.java
+++ Teste/src/br/teste/Teste.java
@@ -1,10 +1,16 @@
 package br.teste;
 
 public class Teste {
+	
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		System.out.println("TEste..");
+		
 	}
 
 }
